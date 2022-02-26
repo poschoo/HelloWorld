60d0b55c88bfc64e7664c2038718d63ac786c0b8
poschoo <poglits.kevin@gmail.com>
2022-02-26 12:29:56 +0100

    init

---
+++ src/VerheiratetOderNicht.java
@@ -0,0 +1,13 @@
+public class VerheiratetOderNicht {
+    public static void main(String[] args) {
+        boolean isMarried = false;
+        if (isMarried){
+            System.out.println("Du bist verheiratet");
+        }
+       else{
+           System.out.println("Du bist nicht verheiratet");
+        }
+
+
+    }
+}

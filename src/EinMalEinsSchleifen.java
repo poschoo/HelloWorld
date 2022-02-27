19495d7f9e8bd849f0f9199897a9d38f8858059d
poschoo <poglits.kevin@gmail.com>
2022-02-27 14:54:05 +0100

    init

---
+++ src/EinMalEinsSchleifen.java
@@ -0,0 +1,16 @@
+public class EinMalEinsSchleifen {
+    public static void main(String[] args) {
+        for(int x=1; x<11; x++){
+//            int y=1;
+//            System.out.println(y + " x " + x + " = " + (y*x));
+            for(int y=1;y<=10;y++) {
+                System.out.println(y + " x " + x + " = " + (y * x));
+            }
+        }
+
+
+
+
+    }
+
+}
